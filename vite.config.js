84da954bb507da3636c1ff565f2a5481c9359cf3
hrefIT <29092010andrusha@gmail.com>
2025-07-07 22:06:35 +0300

    completed4

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'order-summary-component-main/settings',
   plugins: [react()],
 })
